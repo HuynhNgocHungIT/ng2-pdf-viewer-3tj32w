af8dc9c10095cbbe4d304fd345b0b881798d5d4c
HuynhNgocHungIT <99779104+HuynhNgocHungIT@users.noreply.github.com>
2023-07-05 09:32:57 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ng2-pdf-viewer-3tj32w
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ng2-pdf-viewer-3tj32w)
---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  src =
+    'https://static1.cafeland.vn/cafelandnew/upload/file/mauvanban/2020/03/tuan-01/hop-dong-thue-nha.pdf';
+}
